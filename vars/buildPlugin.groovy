e909f3f8342d723bf817ef75572251b4f3124579
Steve Diesner <sgdiesner@outlook.com>
2017-06-25 16:02:08 +0100

    dsl

---
+++ vars/buildPlugin.groovy
@@ -0,0 +1,12 @@
+def call(body) {
+    // evaluate the body block, and collect configuration into the object
+    def config = [:]
+    body.resolveStrategy = Closure.DELEGATE_FIRST
+    body.delegate = config
+    body()
+
+    // now build, based on the configuration provided
+    node {
+        bat "echo Hello from DSL land"
+    }
+}
